c5503d7657798db7e8c4f4c7c9f63cbe711c41cf
Patrick Perry <patperry@stanford.edu>
2008-12-13 14:43:41 -0800

    Add INLINE pragmas to sampling functions

--- lib/Control/Monad/MC/Sample.hs
+++ lib/Control/Monad/MC/Sample.hs
@@ -25,6 +25,7 @@ import Control.Monad.MC.Base
 
 sample :: (MonadMC m) => n -> [a] -> m a
 sample = undefined
+{-# INLINE sample #-}
 
 -- | @sampleInt n@ samples integers uniformly from @[ 0..n-1 ]@.  It is an
 -- error to call this function with a non-positive @n@.
@@ -35,6 +36,7 @@ sampleInt n | n < 1     = fail "invalid argument"
 
 sampleWithWeights :: (MonadMC m) => [Double] -> Int -> [a] -> m a
 sampleWithWeights = undefined
+{-# INLINE sampleWithWeights #-}
 
 -- | @sampleIntWithWeights ws n@ samples integers from @[ 0..n-1 ]@ with the
 -- probability of choosing @i@ proportional to @ws !! i@.  The list @ws@ must
@@ -42,15 +44,20 @@ sampleWithWeights = undefined
 -- with at least one nonzero entry.
 sampleIntWithWeights :: (MonadMC m) => [Double] -> Int -> m Int
 sampleIntWithWeights = undefined
+{-# INLINE sampleIntWithWeights #-}
 
 sampleSubset :: (MonadMC m) => Int -> Int -> [a] -> m [a]
 sampleSubset = undefined
+{-# INLINE sampleSubset #-}
 
 sampleIntSubset :: (MonadMC m) => Int -> Int -> m [Int]
 sampleIntSubset = undefined
+{-# INLINE sampleIntSubset #-}
 
 sampleSubsetWithWeights :: (MonadMC m) => [Double] -> Int -> Int -> [a] -> m [a]
 sampleSubsetWithWeights = undefined
+{-# INLINE sampleSubsetWithWeights #-}
 
 sampleIntSubsetWithWeights :: (MonadMC m) => [Double] -> Int -> Int -> m [Int]
 sampleIntSubsetWithWeights = undefined
+{-# INLINE sampleIntSubsetWithWeights #-}
