39e774ad6136f7dc21d93034734ccc2f3dad1b8a
Patrick Perry <patperry@stanford.edu>
2008-12-14 16:38:19 -0800

    Export Summary data type and functions.

--- lib/Control/Monad/MC/Repeat.hs
+++ lib/Control/Monad/MC/Repeat.hs
@@ -11,56 +11,23 @@ module Control.Monad.MC.Repeat (
     -- * Averaging functions
     repeatMC,
     repeatMCWith,
+    
+    module Control.Monad.MC.Summary,
     ) where
 
 import Control.Monad
 import Control.Monad.MC.Base
+import Control.Monad.MC.Summary
 import Data.List( foldl' )
 
-data Summary = S {-# UNPACK #-} !Int
-                 {-# UNPACK #-} !Double
-                 {-# UNPACK #-} !Double
-
-toDouble :: Int -> Double
-toDouble = realToFrac . toInteger
-
-initSummary :: Summary
-initSummary = S 0 0 0
-
--- | Running mean and variance.  See Knuth, Vol 2, page 232, 3rd edition,
--- or http://www.johndcook.com/standard_deviation.html.
-update :: Summary -> Double -> Summary
-update (S n m s) x =
-    let n'    = n+1
-        delta = x - m
-        m'    = m + delta / toDouble n'
-        s'    = s + delta*(x - m')
-    in S n' m' s'
-
-sampleSize :: Summary -> Int
-sampleSize (S n _ _) = n
-
-sampleMean :: Summary -> Double
-sampleMean (S _ m _) = m
-
-sampleVar :: Summary -> Double
-sampleVar (S n _ s) = s / toDouble (n - 1)
-
-sampleSE :: Summary -> Double
-sampleSE s = sqrt (sampleVar s) / toDouble (sampleSize s)
-
 -- | Repeat a Monte Carlo generator the given number of times and return
--- the sample mean and standard error.  Note that this only works with
+-- the sample summary statistics.  Note that this only works with
 -- @Double@s.
 repeatMC :: (MonadMC m)
          => Int
          -> m Double
-         -> m (Double,Double)
-repeatMC n mb = do
-    s <- repeatMCWith update initSummary n mb
-    let m  = sampleMean s
-        se = sampleSE s
-    m `seq` se `seq` return (m,se)
+         -> m Summary
+repeatMC = repeatMCWith update summary
 {-# INLINE repeatMC #-}
 
 -- | Generalized version of 'repeatMC'.  Run a Monte Carlo generator
---
+++ lib/Control/Monad/MC/Summary.hs
@@ -0,0 +1,81 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Control.Monad.MC.Summary
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module Control.Monad.MC.Summary (
+    -- * Summary statistics
+    -- ** The @Summary@ data type
+    Summary,
+    summary,
+    update,
+    
+    -- ** @Summary@ properties
+    sampleSize,
+    sampleMean,
+    sampleVar,
+    sampleSD,
+    sampleSE,
+    sampleMin,
+    sampleMax,
+    
+    ) where
+
+-- | A type for storing summary statistics for a data set including
+-- sample size, min and max values, and first and second moments.
+data Summary = S {-# UNPACK #-} !Int     -- sample size
+                 {-# UNPACK #-} !Double  -- sample mean
+                 {-# UNPACK #-} !Double  -- sum of squares
+                 {-# UNPACK #-} !Double  -- sample min
+                 {-# UNPACK #-} !Double  -- sample max
+    deriving (Eq)
+    
+-- | Get an empty summary.
+summary :: Summary
+summary = S 0 0 0 (1/0) (-1/0)
+
+-- | Update the summary with a data point.  
+-- Running mean and variance computed as in Knuth, Vol 2, page 232, 
+-- 3rd edition, see http://www.johndcook.com/standard_deviation.html for
+-- a description.
+update :: Summary -> Double -> Summary
+update (S n m s l h) x =
+    let n'    = n+1
+        delta = x - m
+        m'    = m + delta / fromIntegral n'
+        s'    = s + delta*(x - m')
+        l'    = if x < l then x else l
+        h'    = if x > h then x else h
+    in S n' m' s' l' h'
+
+-- | Get the sample size.
+sampleSize :: Summary -> Int
+sampleSize (S n _ _ _ _) = n
+
+-- | Get the sample mean.
+sampleMean :: Summary -> Double
+sampleMean (S _ m _ _ _) = m
+
+-- | Get the sample variance.
+sampleVar :: Summary -> Double
+sampleVar (S n _ s _ _) = s / fromIntegral (n - 1)
+
+-- | Get the sample standard deviation.
+sampleSD :: Summary -> Double
+sampleSD s = sqrt (sampleVar s)
+
+-- | Get the sample standard error.
+sampleSE :: Summary -> Double
+sampleSE s = (sampleSD s) / fromIntegral (sampleSize s)
+
+-- | Get the minimum of the sample.
+sampleMin :: Summary -> Double
+sampleMin (S _ _ _ l _) = l
+
+-- | Get the maximum of the sample.
+sampleMax :: Summary -> Double
+sampleMax (S _ _ _ _ h) = h
