fe313813d9dc939ab275b910255e1988d2b0175a
Patrick Perry <patperry@stanford.edu>
2008-12-21 00:37:27 -0800

    Clean up Poker example

--- examples/Poker.hs
+++ examples/Poker.hs
@@ -8,16 +8,22 @@ import qualified Data.Map as Map
 import System.Environment
 import Text.Printf
     
--- | First we define data types for representing cards.  An Ace has
--- 'number' equal to @1@.  Jack, Queen, and King have numbers @11@, 
--- @12@, and @13@, respectively.
-data Suit = Club | Diamond  | Heart | Spade deriving (Eq, Show, Ord)
+-- | Data types for representing cards.  An Ace has 'number' equal to @1@.
+-- Jack, Queen, and King have numbers @11@, @12@, and @13@, respectively.
+data Suit = Club | Diamond  | Heart | Spade deriving (Eq, Show)
 data Card = Card { number :: !Int 
                  , suit   :: !Suit
                  }
-          deriving (Eq, Show, Ord)
+          deriving (Eq, Show)
 
--- | Now we define a type for the various poker hands
+-- | The number values of the aces and face cards.
+ace, jack, queen, king :: Int
+ace   = 1
+jack  = 11
+queen = 12
+king  = 13
+
+-- | A type for the various poker hands.
 data Hand = HighCard  | Pair | TwoPair | ThreeOfAKind | Straight | Flush
           | FullHouse | FourOfAKind | StraightFlush 
           deriving (Eq, Show, Ord)
@@ -37,34 +43,33 @@ hand cs =
         [2,3]                                        -> FullHouse
         [1,4]                                        -> FourOfAKind
   where
-    isStraight =
-        let c':cs' = sort cs in
-        isStraight' cs' && (  number c' == number (head cs') - 1
-                           || number c' == 1 && number (head cs') == 10)
-      where
-        isStraight' []                            = True
-        isStraight' [x]                           = True
-        isStraight' (x1:x2:xs) = 
-            number x2 == number x1 + 1 && isStraight' (x2:xs)
-        
-    isFlush = all (== suit (head cs)) $ map suit (tail cs)
+    (x:xs) = (sort . map number) cs
+    (s:ss) = map suit cs
     
-    matches = (sort . map length . group . sort . map number) cs
+    isStraight | x == ace && xs == [ 10..king ] = True
+               | otherwise                      = xs == [ x+1..x+4 ]
+
+    isFlush = all (== s) ss
+
+    matches = (sort . map length . group) (x:xs)
+
     
 -- | Get a list of cards that make up a 52-card deck.
 deck :: [Card]
-deck = [ Card i s | i <- [1..13], s <- [ Club, Diamond, Heart, Spade ] ]
+deck = [ Card i s | i <- [ 1..13 ], s <- [ Club, Diamond, Heart, Spade ] ]
 
--- | We deal a five-card hand by choosing a random subset of the deck.
+-- | Deal a five-card hand by choosing a random subset of the deck.
 deal :: (MonadMC m) => m [Card]
 deal = sampleSubset 5 52 deck
 
-
+-- | A type for storing the frequencies of the various hands.
 type HandCounts = Map Hand Int
 
+-- | An empty frequency count.
 emptyCounts :: HandCounts
 emptyCounts = Map.empty
 
+-- | Update the count of the hand corresponding to a list of five cards.
 updateCounts :: HandCounts -> [Card] -> HandCounts
 updateCounts counts cs = Map.insertWith' (+) (hand cs) 1 counts
 
@@ -82,13 +87,10 @@ main' reps =
         "    Hand" " Count" "Probability" "  95% Interval"
     printf "-------------------------------------------------------\n"
     forM_ ((reverse . Map.toAscList) counts) $ \(h,c) ->
-        let p     = fromIntegral c / fromIntegral reps
-            (l,u) = interval p in
-        printf "%-13s %7d   %.8f  (%.6f,%.6f)\n" (show h) c p l u
+        let n     = fromIntegral reps :: Double
+            p     = fromIntegral c / n 
+            se    = sqrt (p * (1 - p) / n)
+            delta = 1.959964 * se
+            (l,u) = (p-delta, p+delta) in
+        printf "%-13s %7d    %.6f   (%.6f,%.6f)\n" (show h) c p l u
     printf "\n"
-  where
-    interval p = 
-        let se    = sqrt (p * (1 - p) / fromIntegral reps)
-            delta = 1.959964 * se in
-        (max (p-delta) 0, p+delta) :: (Double,Double)
-            
