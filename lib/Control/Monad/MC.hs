4e64162ce1a488696dff75005acc278259070955
Patrick Perry <patperry@gmail.com>
2010-09-16 14:06:04 -0400

    Doc update

--- lib/Control/Monad/MC.hs
+++ lib/Control/Monad/MC.hs
@@ -6,8 +6,10 @@
 -- Maintainer : Patrick Perry <patperry@stanford.edu>
 -- Stability  : experimental
 --
--- A monad and monad transformer for monte carlo computations built on top
--- of the functions in the GNU Scientific Library.
+-- A monad and monad transformer for monte carlo computations.  Currently,
+-- the default is the GNU Scientific Library-based implementation, but this
+-- may change in the future.
+--
 
 module Control.Monad.MC (
     module Control.Monad.MC.GSL
