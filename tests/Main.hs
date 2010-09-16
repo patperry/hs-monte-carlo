0caba81f2f46141c54bc68fb27026cec19a134b1
Patrick Perry <patperry@gmail.com>
2010-09-16 13:37:17 -0400

    Port tests to test-framework

--- tests/Main.hs
+++ tests/Main.hs
@@ -10,6 +10,8 @@ import System.Environment
 import System.Random
 import Text.Printf
 import Test.QuickCheck
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
 
 import Control.Monad.MC.Walker
 
@@ -25,9 +27,10 @@ prop_table_index (Weights n ws) (Unif u) =
         i     = indexTable table u
     in i >= 0 && i < n && (ws !! i > 0)
 
-tests_Walker = [ ("table probabilities", mytest prop_table_probs)
-               , ("table indexing"     , mytest prop_table_index)
-               ]
+tests_Walker = testGroup "Walker"
+    [ testProperty "table probabilities" prop_table_probs
+    , testProperty "table indexing"      prop_table_index
+    ]
 
 probOf table i =
     (((sum . map ((1-) . fst) . filter ((==i) . snd))
@@ -70,103 +73,14 @@ unif = do
 data Weights = Weights Int [Double] deriving Show
 instance Arbitrary Weights where
     arbitrary = do
-        n  <- posInt
+        n  <- choose (1, 500)
         ws <- weights n
         return $ Weights n ws
-
-    coarbitrary (Weights n ws) =
-        coarbitrary (n,ws)
 
 data Unif = Unif Double deriving Show
 instance Arbitrary Unif where
     arbitrary            = liftM Unif unif
-    coarbitrary (Unif u) = coarbitrary u
-
-------------------------------------------------------------------------
---
--- QC driver ( taken from xmonad-0.6 )
---
-
-debug = False
-
-mytest :: Testable a => a -> Int -> IO (Bool, Int)
-mytest a n = mycheck defaultConfig
-    { configMaxTest=n
-    , configEvery   = \n args -> let s = show n in s ++ [ '\b' | _ <- s ] } a
- -- , configEvery= \n args -> if debug then show n ++ ":\n" ++ unlines args else [] } a
-
-mycheck :: Testable a => Config -> a -> IO (Bool, Int)
-mycheck config a = do
-    rnd <- newStdGen
-    mytests config (evaluate a) rnd 0 0 []
-
-mytests :: Config -> Gen Result -> StdGen -> Int -> Int -> [[String]] -> IO (Bool, Int)
-mytests config gen rnd0 ntest nfail stamps
-    | ntest == configMaxTest config = done "OK," ntest stamps >> return (True, ntest)
-    | nfail == configMaxFail config = done "Arguments exhausted after" ntest stamps >> return (True, ntest)
-    | otherwise               =
-      do putStr (configEvery config ntest (arguments result)) >> hFlush stdout
-         case ok result of
-           Nothing    ->
-             mytests config gen rnd1 ntest (nfail+1) stamps
-           Just True  ->
-             mytests config gen rnd1 (ntest+1) nfail (stamp result:stamps)
-           Just False ->
-             putStr ( "Falsifiable after "
-                   ++ show ntest
-                   ++ " tests:\n"
-                   ++ unlines (arguments result)
-                    ) >> hFlush stdout >> return (False, ntest)
-     where
-      result      = generate (configSize config ntest) rnd2 gen
-      (rnd1,rnd2) = split rnd0
-
-done :: String -> Int -> [[String]] -> IO ()
-done mesg ntest stamps = putStr ( mesg ++ " " ++ show ntest ++ " tests" ++ table )
-  where
-    table = display
-            . map entry
-            . reverse
-            . sort
-            . map pairLength
-            . group
-            . sort
-            . filter (not . null)
-            $ stamps
-
-    display []  = ".\n"
-    display [x] = " (" ++ x ++ ").\n"
-    display xs  = ".\n" ++ unlines (map (++ ".") xs)
-
-    pairLength xss@(xs:_) = (length xss, xs)
-    entry (n, xs)         = percentage n ntest
-                       ++ " "
-                       ++ concat (intersperse ", " xs)
-
-    percentage n m        = show ((100 * n) `div` m) ++ "%"
-
-------------------------------------------------------------------------
-
 
 
 main :: IO ()
-main = do
-    args <- getArgs
-    let n = if null args then 100 else read (head args)
-
-    (results, passed) <- liftM unzip $
-        foldM ( \prev (name,subtests) -> do
-                     printf "\n%s\n" name
-                     printf "%s\n" $ replicate (length name) '-'
-                     cur <- mapM (\(s,a) -> printf "%-30s: " s >> a n) subtests
-                     return (prev ++ cur)
-              )
-              []
-              tests
-
-    printf "\nPassed %d tests!\n\n" (sum passed)
-    when (not . and $ results) $ fail "\nNot all tests passed!"
- where
-
-    tests = [ ("Walker"  , tests_Walker)
-            ]
+main = defaultMain [ tests_Walker ]
