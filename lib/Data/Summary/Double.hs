63349c34c80138ab957a18733045a1bd64dafdb2
Clark Gaebel <cg.wowus.cg@gmail.com>
2013-04-20 17:30:11 -0400

    Added the missing unbox instance.

--- lib/Data/Summary/Double.hs
+++ lib/Data/Summary/Double.hs
@@ -112,6 +112,8 @@ instance GM.MVector U.MVector Summary where
         v' <- GM.basicUnsafeGrow v i
         return $ MV_Summary v'
 
+instance U.Unbox Summary
+
 -- | Get a summary of a list of values.
 summary :: [Double] -> Summary
 summary = foldl' update empty
